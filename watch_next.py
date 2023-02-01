f3e12964e4ce15dce55bef277acbc672407c01aa
elie <fernickelie@gmail.com>
2023-02-01 00:44:52 +0000

    Added task description and space before # and comments

--- watch_next.py
+++ watch_next.py
@@ -1,21 +1,32 @@
+# Note - Version 2 - considers two
+'''
+For this version, 2 correctionas were made
+* we take movie description as an argument
+* We also return one most similar movie
+
+'''
+
+
+# Task description
 '''
 For this task we build a movie recommender
 The system will recommend top movie given the similarity
 We call this program: Hyperion Netflix!
 '''
-#import needed librari
+
+# import needed librari
 import spacy
 nlp = spacy.load('en_core_web_md')
 
 
-#opening movie data
+# opening movie data
 movie_list=[]
 with open("movies.txt","r") as f:
     for line in f.readlines():
         line_stripped= line.strip('\n')
         movie_list.append(line_stripped)
 
-#movie reference description to compare
+# movie reference description to compare
 hulk = '''Will he save their world or destroy it? When the Hulk becomes too dangerous for the
             Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a
             planet where the Hulk can live in peace. Unfortunately, Hulk land on the
@@ -23,10 +34,10 @@ hulk = '''Will he save their world or destroy it? When the Hulk becomes too dang
 
 to_compare = nlp(hulk)
 
-#find similarity score
+# find similarity score
 
 
-#defining a function to collect similarity score
+# defining a function to collect similarity score
 similarity_score = []
 def score_collector():
     
@@ -35,7 +46,7 @@ def score_collector():
         similarity_score.append(similarity)
     
 
-#finding the top 4
+# finding the top 4
 def get_top(to_be_compared):
     score_collector()
     print("\nYou might like this movie as well.")
@@ -52,7 +63,7 @@ def get_top(to_be_compared):
 
     print("\nThank you for using Hyperion Netflix!!\n")
 
-#Start of the program to get the top movie
+# Start of the program to get the top movie
 get_top(to_compare)
 
 
